1b717cb234a019d40ddc7046ef78cb3160c716b3
hamzaejaz787 <hejaz389@gmail.com>
2024-04-26 12:43:34 +0500

    content updated

--- components/FiveCardsSection.tsx
+++ components/FiveCardsSection.tsx
@@ -20,28 +20,26 @@ const FiveCardsSection = ({
   fiveCardsData: fiveCardsDataProps[];
 }) => {
   return (
-    <Suspense fallback={<Loading />}>
-      <section className="py-8 sm:py-12 px-8 sm:px-10 md:px-20 space-y-4 container">
-        <h2 className="text-custom-purple-300 uppercase text-4xl font-tommy">
-          {fiveCardsSectionCotent.title}
-        </h2>
-        <p>{fiveCardsSectionCotent.description}</p>
+    <section className="py-8 sm:py-12 px-8 sm:px-10 md:px-20 space-y-4 container">
+      <h2 className="text-custom-purple-300 uppercase text-4xl font-tommy">
+        {fiveCardsSectionCotent.title}
+      </h2>
+      <p>{fiveCardsSectionCotent.description}</p>
 
-        <div className="pt-8 flex flex-wrap justify-center gap-8 w-full">
-          {fiveCardsData.map((item) => (
-            <Card
-              key={item.title}
-              className="p-8 w-full max-w-xs md:max-w-[400px] flex items-center gap-4 flex-col justify-center border-none text-center bg-custom-purple-500"
-            >
-              <CardTitle className="text-white">{item.title}</CardTitle>
-              <CardDescription className="text-white">
-                {item.description}
-              </CardDescription>
-            </Card>
-          ))}
-        </div>
-      </section>
-    </Suspense>
+      <div className="pt-8 flex flex-wrap justify-center gap-8 w-full">
+        {fiveCardsData.map((item) => (
+          <Card
+            key={item.title}
+            className="p-8 w-full max-w-xs md:max-w-[400px] flex items-center gap-4 flex-col justify-center border-none text-center bg-custom-purple-500"
+          >
+            <CardTitle className="text-white">{item.title}</CardTitle>
+            <CardDescription className="text-white">
+              {item.description}
+            </CardDescription>
+          </Card>
+        ))}
+      </div>
+    </section>
   );
 };
 
--- components/GetInTouch.tsx
+++ components/GetInTouch.tsx
@@ -8,68 +8,66 @@ import Loading from "@/app/loading";
 
 const GetInTouch = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <section className="bg-custom-purple-500 text-white py-12 px-6 sm:px-12 md:px-20">
-        <strong className="uppercase text-center block">Connect</strong>
-        <h2 className="text-center text-5xl font-bold">Get In Touch</h2>
-        <p className="text-center text-custom-purple-100">
-          We&apos;re here to answer any questions you may have. Feel free to
-          reach out to us.
-        </p>
-        <div className="container px-0 mt-8 flex items-start justify-between gap-8 md:gap-20  flex-wrap md:flex-nowrap">
-          <GetInTouchForm />
+    <section className="bg-custom-purple-500 text-white py-12 px-6 sm:px-12 md:px-20">
+      <strong className="uppercase text-center block">Connect</strong>
+      <h2 className="text-center text-5xl font-bold">Get In Touch</h2>
+      <p className="text-center text-custom-purple-100">
+        We&apos;re here to answer any questions you may have. Feel free to reach
+        out to us.
+      </p>
+      <div className="container px-0 mt-8 flex items-start justify-between gap-8 md:gap-20  flex-wrap md:flex-nowrap">
+        <GetInTouchForm />
 
-          <div className="space-y-4">
-            <div className="flex items-center gap-4">
-              <span className="p-2 bg-custom-purple-300/50 rounded-md">
-                <MdEmail size={25} className="text-white" />
-              </span>
+        <div className="space-y-4">
+          <div className="flex items-center gap-4">
+            <span className="p-2 bg-custom-purple-300/50 rounded-md">
+              <MdEmail size={25} className="text-white" />
+            </span>
 
-              <div>
-                <h5 className="text-lg font-bold">Email</h5>
-                <Link
-                  className="hover:text-custom-purple-100 transition-all duration-200"
-                  href="mailto:info@BigO.com"
-                >
-                  info@BigO.com
-                </Link>
-              </div>
+            <div>
+              <h5 className="text-lg font-bold">Email</h5>
+              <Link
+                className="hover:text-custom-purple-100 transition-all duration-200"
+                href="mailto:info@BigO.com"
+              >
+                info@BigO.com
+              </Link>
             </div>
-            <div className="flex items-center gap-4">
-              <span className="p-2 bg-custom-purple-300/50 rounded-md">
-                <FaPhoneAlt size={25} className="text-white" />
-              </span>
+          </div>
+          <div className="flex items-center gap-4">
+            <span className="p-2 bg-custom-purple-300/50 rounded-md">
+              <FaPhoneAlt size={25} className="text-white" />
+            </span>
 
-              <div>
-                <h5 className="text-lg font-bold">Phone</h5>
-                <Link
-                  className="hover:text-custom-purple-100 transition-all duration-200"
-                  href="tel:+923165143252"
-                >
-                  +92 316 514 3252
-                </Link>
-              </div>
+            <div>
+              <h5 className="text-lg font-bold">Phone</h5>
+              <Link
+                className="hover:text-custom-purple-100 transition-all duration-200"
+                href="tel:+923165143252"
+              >
+                +92 316 514 3252
+              </Link>
             </div>
-            <div className="flex items-center gap-4">
-              <span className="p-2 bg-custom-purple-300/50 rounded-md">
-                <FaLocationDot size={25} className="text-white" />
-              </span>
+          </div>
+          <div className="flex items-center gap-4">
+            <span className="p-2 bg-custom-purple-300/50 rounded-md">
+              <FaLocationDot size={25} className="text-white" />
+            </span>
 
-              <div>
-                <h5 className="text-lg font-bold">Office</h5>
-                <Link
-                  className="hover:text-custom-purple-100 transition-all duration-200"
-                  href="https://maps.app.goo.gl/2L9f9WYFS6Anjjg39"
-                  target="_blank"
-                >
-                  First Floor, Plaza #54, Sector F, DHA Phase 1, Rawalpindi
-                </Link>
-              </div>
+            <div>
+              <h5 className="text-lg font-bold">Office</h5>
+              <Link
+                className="hover:text-custom-purple-100 transition-all duration-200"
+                href="https://maps.app.goo.gl/2L9f9WYFS6Anjjg39"
+                target="_blank"
+              >
+                First Floor, Plaza #54, Sector F, DHA Phase 1, Rawalpindi
+              </Link>
             </div>
           </div>
         </div>
-      </section>
-    </Suspense>
+      </div>
+    </section>
   );
 };
 
